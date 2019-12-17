7b29869bdb2ac441fc6de7fc7ebd65ab08a212bf
Christopher Buecheler <cwbuecheler@gmail.com>
2019-12-17 13:07:20 -0500

    Tutorial 12

---
+++ intermediate_steps/12-component_detailscreen/src/components/home/HomeScreenContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import HomeScreen from './HomeScreen';
+
+export default class HomeScreenContainer extends React.Component {
+  render() {
+    return <HomeScreen />;
+  }
+}
